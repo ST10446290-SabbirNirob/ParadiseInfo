69f0269a633842a10106e3585cfb912b34aabcfe
ST10446290-SabbirNirob <ST10446290@vcconnect.edu.za>
2024-03-19 13:39:14 +0200

    ParadiseInfo
    ParadiseInfo calling class

---
+++ src/paradiseinfo/ParadiseInfo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package paradiseinfo;
+
+/**
+ *
+ * @author Lucifer
+ */
+public class ParadiseInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        displayInfo();
+    
+    }
+    public static void displayInfo(){
+        System.out.println("Paradise Day Spa whats to pamper you.");
+        System.out.println("We will make you look good."); 
+    }
+    
+}
